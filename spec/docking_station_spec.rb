cedc26f03107084895ceb1433328189e66327853
Ashwini <ashie.oh@gmail.com>
2017-02-14 18:39:40 +0000

    Second unit test is working

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,7 +2,7 @@ require 'docking_station'
 
 describe DockingStation do
   describe "responds to release_bike" do
-    it {respond_to? :release_bike}
+    it {is_expected.to respond_to :release_bike}
     #it 'responds to release_bike method' do
     # expect(DockingStation.new.respond_to?(:release_bike)).to eq true
     # expect(subject).to respond_to :release_bike
