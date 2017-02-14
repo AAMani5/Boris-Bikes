36c3943674fb1092742128c2edbf4afb912361c2
Ashwini <ashie.oh@gmail.com>
2017-02-14 16:20:05 +0000

    First unit test is working

---
+++ spec/docking_station_spec.rb
@@ -0,0 +1,7 @@
+require 'docking_station'
+
+describe DockingStation do
+#  it 'returns a new Bike when release_bike is called' do
+  #  expect(DockingStation.release_bike).to eq 'Bike'
+#  end
+end
