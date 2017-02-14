24239ae7ef8e76228c047e5ecb19b0e205640175
Ashwini <ashie.oh@gmail.com>
2017-02-14 18:49:45 +0000

    Added a few more testing techniques

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,5 +1,9 @@
 require 'bike'
 
 describe Bike do
-  it {is_expected.to respond_to :working? }
+   it {is_expected.to respond_to :working? }
+  # it 'responds to release_bike method' do
+   # expect(Bike.new.respond_to?(:working?)).to eq true
+   # expect(subject).to respond_to :working?
+  end
 end
