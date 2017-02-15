ac249f293dfb2db4bd65b79dde6391a5655d7e99
Ashwini <ashie.oh@gmail.com>
2017-02-15 12:55:06 +0000

    added and tested a feature

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,7 +1,7 @@
 require 'bike'
 
 describe Bike do
-   #it {is_expected.to respond_to :working? } # one-liner
+   it {is_expected.to respond_to :working? } # one-liner
    # normal testing technique
   # it 'responds to release_bike method' do
    # expect(Bike.new.respond_to?(:working?)).to eq true
